860fbd89864dc6268a2cd72f32292b5d2999f4e2
Luka Milic <lukam321@gmail.com>
2024-03-27 22:28:33 +0000

    tidy up new model

--- src/subsai/configs.py
+++ src/subsai/configs.py
@@ -65,9 +65,7 @@ AVAILABLE_MODELS = {
     },
     'API/openai/whisper': {
         'class': WhisperAPIModel,
-        'description': 'Whisper is a general-purpose speech recognition model. It is trained on a large dataset of '
-                       'diverse audio and is also a multi-task model that can perform multilingual speech recognition '
-                       'as well as speech translation and language identification.',
+        'description': 'API variant of the OpenAI whisper model, just requires an api key',
         'url': 'https://github.com/openai/whisper',
         'config_schema': WhisperAPIModel.config_schema,
     },
